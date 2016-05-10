c55ef480a7647c3354c28868e4f7f42dc68a47d4
Eric <ericluna1992@gmail.com>
2016-05-09 19:35:12 -0500

    testing scroll effects

--- script.js
+++ script.js
@@ -34,8 +34,8 @@ $(document).ready(function(){
 	$(window).scroll(function() {
 		// Scroll effects for the about section at the top of page
 		if ($(document).scrollTop() > 0 && $(document).scrollTop() < ($(".html5").offset().top-165)) {
-			$(".about-div").addClass("stuff");
-			$(".about").addClass("things");
+			$(".about-div").addClass("about-div-scroll");
+			$(".about").addClass("about-scroll");
 		}else{
 			$(".about-div").removeClass("stuff");
 			$(".about").removeClass("things");
@@ -65,7 +65,7 @@ $(document).ready(function(){
 
 		// Scroll effects for the project section.
 		
-		if ($(document).scrollTop() > ($(".p1").offset().top-170) && $(document).scrollTop() < ($(".p2").offset().top)-85) {
+		if ($(document).scrollTop() > ($(".p1").offset().top-165) && $(document).scrollTop() < ($(".p2").offset().top)-165) {
 			$(".p1 .project-pic").addClass("project-pic-scroll");
 			$(".p1 .project-info h1").addClass("project-h1-scroll");
 			$(".p1 .project-info p").addClass("project-description-scroll");
@@ -75,7 +75,7 @@ $(document).ready(function(){
 			$(".p1 .project-info p").removeClass("project-description-scroll");
 		}
 
-		if ($(document).scrollTop() > ($(".p2").offset().top-170) && $(document).scrollTop() < ($(".p3").offset().top)-165) {
+		if ($(document).scrollTop() > ($(".p2").offset().top-165) && $(document).scrollTop() < ($(".p3").offset().top)-165) {
 			$(".p2 .project-pic").addClass("project-pic-scroll");
 			$(".p2 .project-info h1").addClass("project-h1-scroll");
 			$(".p2 .project-info p").addClass("project-description-scroll");
@@ -85,7 +85,7 @@ $(document).ready(function(){
 			$(".p2 .project-info p").removeClass("project-description-scroll");
 		}
 
-		if ($(document).scrollTop() > ($(".p3").offset().top-170) && $(document).scrollTop() < ($(".p4").offset().top)-165) {
+		if ($(document).scrollTop() > ($(".p3").offset().top-165) && $(document).scrollTop() < ($(".p4").offset().top)-165) {
 			$(".p3 .project-pic").addClass("project-pic-scroll");
 			$(".p3 .project-info h1").addClass("project-h1-scroll");
 			$(".p3 .project-info p").addClass("project-description-scroll");
@@ -95,7 +95,7 @@ $(document).ready(function(){
 			$(".p3 .project-info p").removeClass("project-description-scroll");
 		}
 
-		if ($(document).scrollTop() > ($(".p4").offset().top-170) && $(document).scrollTop() < ($(".p5").offset().top)-165) {
+		if ($(document).scrollTop() > ($(".p4").offset().top-165) && $(document).scrollTop() < ($(".p5").offset().top)-165) {
 			$(".p4 .project-pic").addClass("project-pic-scroll");
 			$(".p4 .project-info h1").addClass("project-h1-scroll");
 			$(".p4 .project-info p").addClass("project-description-scroll");
@@ -105,7 +105,7 @@ $(document).ready(function(){
 			$(".p4 .project-info p").removeClass("project-description-scroll");
 		}
 
-		if ($(document).scrollTop() > ($(".p5").offset().top-170) && $(document).scrollTop() < ($(".p6").offset().top)-165) {
+		if ($(document).scrollTop() > ($(".p5").offset().top-165) && $(document).scrollTop() < ($(".p6").offset().top)-165) {
 			$(".p5 .project-pic").addClass("project-pic-scroll");
 			$(".p5 .project-info h1").addClass("project-h1-scroll");
 			$(".p5 .project-info p").addClass("project-description-scroll");
@@ -115,7 +115,7 @@ $(document).ready(function(){
 			$(".p5 .project-info p").removeClass("project-description-scroll");
 		}
 
-		if ($(document).scrollTop() > ($(".p6").offset().top-170) && $(document).scrollTop() < ($("footer").offset().top)-165) {
+		if ($(document).scrollTop() > ($(".p6").offset().top-165) && $(document).scrollTop() < ($("footer").offset().top)-165) {
 			$(".p6 .project-pic").addClass("project-pic-scroll");
 			$(".p6 .project-info h1").addClass("project-h1-scroll");
 			$(".p6 .project-info p").addClass("project-description-scroll");
@@ -124,10 +124,6 @@ $(document).ready(function(){
 			$(".p6 .project-info h1").removeClass("project-h1-scroll");
 			$(".p6 .project-info p").removeClass("project-description-scroll");
 		}
-
-
-
-
 	});
 
 
