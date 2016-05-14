1a2daf201b672bba81bcddeccbddb7d8ef53ea09
Eric <ericluna1992@gmail.com>
2016-05-13 19:25:51 -0500

    added prevent defaul function to the nav scroll links so the page does not skip around

--- script.js
+++ script.js
@@ -18,17 +18,20 @@ $(document).ready(function(){
 	});
 
 	// This is the code to enable smooth scrolling when nav links are clicked.
-	$(".skill-button").click(function() {
+	$(".skill-button").click(function(e) {
+		e.preventDefault();
 		$('html, body').animate({
 			scrollTop: $("#skills").offset().top
 		}, 2000);
 	});
-	$(".project-button").click(function() {
+	$(".project-button").click(function(e) {
+		e.preventDefault();
 		$('html, body').animate({
 			scrollTop: $("#projects").offset().top
 		}, 2000);
 	});
-	$(".contact-button").click(function() {
+	$(".contact-button").click(function(e) {
+		e.preventDefault();
 		$('html, body').animate({
 			scrollTop: $("#contact").offset().top
 		}, 2000);
