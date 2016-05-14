e3ed3020982868afc8d6defe7b0e2ae003377e5d
Eric <ericluna1992@gmail.com>
2016-05-13 19:16:20 -0500

    added some padding for the mobile links and increaed the line height all over the page

--- script.js
+++ script.js
@@ -6,11 +6,14 @@ $(document).ready(function(){
 	$('#contact').fadeIn(5000);
 
 	// This is the code for the fixed mobile nav bar. 
-	var $mobileNavTrigger = $(".mobile-nav-bar"),
-	$targetMobileNav = $(".mobile-nav"),
-	showMobileNavClass = "show-mobile-nav";
+	var $mobileNavTrigger = $(".mobile-nav-bar");
+	var $targetMobileNav = $(".mobile-nav");
+	var showMobileNavClass = "show-mobile-nav";
 
 	$mobileNavTrigger.click(function(){
+		$targetMobileNav.toggleClass(showMobileNavClass);
+	});
+	$('.mobile-link').click(function(){
 		$targetMobileNav.toggleClass(showMobileNavClass);
 	});
 
@@ -41,21 +44,21 @@ $(document).ready(function(){
 		} 
 
 		// Scroll Effects for each skill section
-		if ($(document).scrollTop() > ($(".html5").offset().top-165) && $(document).scrollTop() < $(".responsive").offset().top-165) {
+		if ($(document).scrollTop() > ($(".html5").offset().top-185) && $(document).scrollTop() < $(".responsive").offset().top-185) {
 			$(".html5").addClass("skill-scroll");
 			$(".html5 .html-pic").addClass("html-pic-scroll");
 		}else{
 			$(".html5").removeClass("skill-scroll");
 			$(".html5 .html-pic").removeClass("html-pic-scroll");
 		}
-		if ($(document).scrollTop() > ($(".responsive").offset().top-165) && $(document).scrollTop() < ($(".javascript").offset().top)-165) {
+		if ($(document).scrollTop() > ($(".responsive").offset().top-185) && $(document).scrollTop() < ($(".javascript").offset().top)-185) {
 			$(".responsive").addClass("skill-scroll");
 			$(".responsive .skill-pic").addClass("pic-scroll");
 		}else{
 			$(".responsive").removeClass("skill-scroll");
 			$(".responsive .skill-pic").removeClass("pic-scroll");
 		}
-		if ($(document).scrollTop() > ($(".javascript").offset().top-165) && $(document).scrollTop() < ($("#projects").offset().top)-165) {
+		if ($(document).scrollTop() > ($(".javascript").offset().top-185) && $(document).scrollTop() < ($("#projects").offset().top)-185) {
 			$(".javascript").addClass("skill-scroll");
 			$(".javascript .skill-pic").addClass("pic-scroll");
 		}else{
@@ -65,20 +68,20 @@ $(document).ready(function(){
 
 		// Scroll effects for the project section.
 		
-		if ($(document).scrollTop() > ($(".p1").offset().top-165) && $(document).scrollTop() < ($(".p2").offset().top)-165) {
+		if ($(document).scrollTop() > ($(".p1").offset().top-185) && $(document).scrollTop() < ($(".p2").offset().top)-185) {
 			$(".p1 .project-pic").addClass("project-pic-scroll");
 			
 		}else{
 			$(".p1 .project-pic").removeClass("project-pic-scroll");
 		}
 
-		if ($(document).scrollTop() > ($(".p2").offset().top-165) && $(document).scrollTop() < ($(".p3").offset().top)-165) {
+		if ($(document).scrollTop() > ($(".p2").offset().top-185) && $(document).scrollTop() < ($(".p3").offset().top)-185) {
 			$(".p2 .project-pic").addClass("project-pic-scroll");
 		}else{
 			$(".p2 .project-pic").removeClass("project-pic-scroll");
 		}
 
-		if ($(document).scrollTop() > ($(".p3").offset().top-165) && $(document).scrollTop() < ($(".p4").offset().top)-165) {
+		if ($(document).scrollTop() > ($(".p3").offset().top-185) && $(document).scrollTop() < ($(".p4").offset().top)-185) {
 			$(".p3 .project-pic").addClass("project-pic-scroll");
 			
 		}else{
@@ -86,7 +89,7 @@ $(document).ready(function(){
 			
 		}
 
-		if ($(document).scrollTop() > ($(".p4").offset().top-165) && $(document).scrollTop() < ($(".p5").offset().top)-165) {
+		if ($(document).scrollTop() > ($(".p4").offset().top-185) && $(document).scrollTop() < ($(".p5").offset().top)-185) {
 			$(".p4 .project-pic").addClass("project-pic-scroll");
 			
 		}else{
@@ -94,20 +97,20 @@ $(document).ready(function(){
 			
 		}
 
-		if ($(document).scrollTop() > ($(".p5").offset().top-165) && $(document).scrollTop() < ($(".p6").offset().top)-165) {
+		if ($(document).scrollTop() > ($(".p5").offset().top-185) && $(document).scrollTop() < ($(".p6").offset().top)-185) {
 			$(".p5 .project-pic").addClass("project-pic-scroll");
 			
 		}else{
 			$(".p5 .project-pic").removeClass("project-pic-scroll");
 		}
-		if ($(document).scrollTop() > ($(".p6").offset().top-165) && $(document).scrollTop() < ($(".p7").offset().top)-165) {
+		if ($(document).scrollTop() > ($(".p6").offset().top-185) && $(document).scrollTop() < ($(".p7").offset().top)-185) {
 			$(".p6 .project-pic").addClass("project-pic-scroll");
 			
 		}else{
 			$(".p6 .project-pic").removeClass("project-pic-scroll");
 		}
 
-		if ($(document).scrollTop() > ($(".p7").offset().top-165) && $(document).scrollTop() < ($("footer").offset().top)-165) {
+		if ($(document).scrollTop() > ($(".p7").offset().top-185) && $(document).scrollTop() < ($("footer").offset().top)-185) {
 			$(".p7 .project-pic").addClass("project-pic-scroll");
 		}else{
 			$(".p7 .project-pic").removeClass("project-pic-scroll");
